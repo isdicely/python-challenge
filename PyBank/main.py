a8bebf994491d445ba341ea5aa7b53f5a33b1138
ISDicely <isdicely@gmail.com>
2021-01-10 00:54:32 -0800

    Calculated greatest profit increase and greatest loss decrease, identified dates for these values.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,15 +16,19 @@ with open(csvpath) as csvfile:
     budget_data_header = next(budget_data)
     print(f"CSV Header: {budget_data_header}")
     # Calculate the number of months in the data set
-    #The net total amount of "Profit/Losses" over the entire period
+    # The net total amount of "Profit/Losses" over the entire period
     # Calculate the changes in "Profit/Losses" over the entire period, 
-    # then find the average of those changes
-    
+        #then find the average of those changes
+    # Find the greatest increase in profits (date and amount) over the entire period
+    # Find the greatest decrease in losses (date and amount) over the entire period
     # num_months will hold the number of months in the data set after running the for loop.
     # mon_year holds the date
     # sum_profits_losses will hold the sum of the profits and losses
-    # delta_profits_losses will hold the difference between the profits and losses
-        # this will help calculate the average of the difference
+    # previous_profit_loss_value, current_profit_loss_value, delta_profits_loss_value,
+        # sum_delta_profits_losses will help calculate the average of the difference
+    # greatest_profit_increase and greatest_loss_decrease varibles to help identify these these values
+        # date_grtst_profit_inc and date_grtst_loss_dec will hold the date when these values occured
+        
     num_months = 0
     mon_year = ""
     sum_profits_losses = 0
@@ -32,6 +36,11 @@ with open(csvpath) as csvfile:
     current_profit_loss_value = 0
     delta_profits_losses = 0
     sum_delta_profits_losses = 0
+    greatest_profit_increase = 0
+    greatest_loss_decrease = 0
+    date_grtst_profit_inc = ""
+    date_grtst_loss_dec = ""
+
     # Iterate through all rows
     for row in budget_data:
         # Count the number of months in the data set
@@ -50,6 +59,13 @@ with open(csvpath) as csvfile:
             sum_delta_profits_losses += delta_profits_losses
         # Reset previos profit/losses variable
         previous_profit_loss_value = current_profit_loss_value
+        # Find the greatest profit and loss values
+        if delta_profits_losses > greatest_profit_increase:
+            greatest_profit_increase = delta_profits_losses
+            date_grtst_profit_inc = mon_year
+        if delta_profits_losses < greatest_loss_decrease:
+            greatest_loss_decrease = delta_profits_losses
+            date_grtst_loss_dec = mon_year
     # Calculate the average of the changes in the profit/losses
     ave_delta_profits_losses = sum_delta_profits_losses/(num_months-1)
 
@@ -57,3 +73,7 @@ with open(csvpath) as csvfile:
     print(num_months)
     print(sum_profits_losses)
     print(ave_delta_profits_losses)
+    print(greatest_loss_decrease)
+    print(date_grtst_loss_dec)
+    print(greatest_profit_increase)
+    print(date_grtst_profit_inc)
